eab3f630fad5ae82b556add7fa6f7f46a7fc9a62
Pepe Valdivia <jvaldivia@softwebe.pe>
2017-11-06 10:41:38 -0500

    Vista del home en php

--- app/routes/home.php
+++ app/routes/home.php
@@ -4,7 +4,7 @@ class Home
 {
 		public static function index()
 		{
-				Flight::render('index/index', array('data' => 'Bob'), 'partial');
+				Flight::render('home/index', array('data' => 'Bob'), 'partial');
 				Flight::render('layouts/app', array('title' => 'Bienvenido'));
 		}
 }
